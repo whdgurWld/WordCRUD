604322b92c678591b585caf266c28f041b9bbc47
whdgurWld <nickyparkg4@gmail.com>
2023-09-05 18:05:06 +0900

    Create WordCRUD Project

---
+++ src/main/java/org/example/WordCRUD.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCRUD implements ICRUD{
+
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+
+    @Override
+    public Object add() {
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord(){
+        Word word = (Word) add();
+        list.add(word);
+        System.out.println("새 단어가 단어장에 추가되었습니다!!!");
+    }
+
+    public void listAll(){
+        System.out.println("-----------------------");
+        for(int i = 0; i < list.size(); i++){
+            System.out.print((i+1) + " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("-----------------------");
+    }
+
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+}
---
+++ src/main/java/org/example/WordManager.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class WordManager {
+
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
+
+
+    public void start() {
+        while(true) {
+            int menu = selectMenu();
+            if (menu == 0) {
+                System.out.println("프로그램 종료! 다음에 만나요~");
+                break;
+            }
+            else if (menu == 1) {
+                //Print all Words
+                wordCRUD.listAll();
+            }
+            else if(menu == 4){
+                //Add new Word
+                wordCRUD.addWord();
+            }
+        }
+    }
+    public int selectMenu(){
+        System.out.print("""
+                *** 영단어 마스터 ***
+                ********************
+                1. 모든 단어 보기
+                2. 수준별 단어 보기
+                3. 단어 검색
+                4. 단어 추가
+                5. 단어 수정
+                6. 단어 삭제
+                7. 파일 저장
+                0. 나가기
+                ********************
+                => 원하는 메뉴는?\s""");
+        return s.nextInt();
+    }
+}
