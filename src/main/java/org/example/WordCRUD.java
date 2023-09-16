37da55ce42192ff354d601649231186c308ce5f4
whdgurWld <nickyparkg4@gmail.com>
2023-09-16 18:57:44 +0900

    Modify WordCRUD.java

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -49,7 +49,7 @@ public class WordCRUD implements ICRUD{
         System.out.println("-----------------------");
         for(int i = 0; i < list.size(); i++){
             String word = list.get(i).getWord();
-            if(!word.startsWith(keyword)) continue;
+            if(!word.contains(keyword)) continue;
 
             System.out.print((j + 1) + " ");
             System.out.println(list.get(i).toString());
